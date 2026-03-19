da81098cc52d7f28dbf81e115b043292fd43fa9c
USER <user123@gmail.com>
2026-03-19 23:19:24 +0530

    fix: added supabase auth helpers

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    turbopack: false, // Disable Turbopack to avoid React static flag errors
-  },
+
   images: {
     remotePatterns: [
       {
